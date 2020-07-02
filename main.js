63e743878431a614e7bb9d65b8654887c40a76b1
emanuele5180 <macina5180>
2020-07-02 16:11:07 +0200

    prova click hamburger menù

---
+++ main.js
@@ -0,0 +1,6 @@
+// cliccando sull'icona hamburger deve apparire il menù
+// quindi il menù right deve andare in display block
+
+$("hamburger-menu").click( function(){
+  $("header-right").show();
+})
