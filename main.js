36064c7006c4de9ea450114d9c421d28692287a6
emanuele5180 <macina5180>
2020-07-02 16:56:39 +0200

    tentativo

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 // cliccando sull'icona hamburger deve apparire il menù
 // quindi il menù right deve andare in display block
 
-$("hamburger-menu").click( function(){
-  $("header-right").show();
-})
+// $("hamburger-menu").click( function(){
+//   $(".header-right ul").show();
+// })
+
+
+$(".hamburger-menu ul").click( function(){
+  $(".hamburger-menu ul").addClass(".hamburger-menu.active");
+}
